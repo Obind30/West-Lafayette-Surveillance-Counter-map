808f5ff5b0510956b86399dd28808a95a94eff59
Obind30 <obindewald30@gmail.com>
2026-04-29 15:00:40 -0400

    Load and Filter Flock Geojson

---
+++ Flock_Geo_Filter.py
@@ -0,0 +1,22 @@
+import json
+
+def filter_by_coord(object):
+    try:
+        coords = object["geometry"]["coordinates"]
+        if coords[0] > -86.962328 and coords[0] < -86.752802 and coords[1] > 40.301640 and coords[1] < 40.603983:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+nationwide_file = open("National-Flock-Data.geojson")
+local_file = open("GreaterLAF-Flock-Cameras.geojson", "w")
+
+camera_data = json.load(nationwide_file)
+
+local_features = list(filter(filter_by_coord, camera_data["features"]))
+
+camera_data["features"] = local_features
+
+json.dump(camera_data, local_file, sort_keys=True, indent=4)
