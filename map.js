7eaa3fc6d45e54b68869f7f2cbfec9f988d92224
Obind30 <obindewald30@gmail.com>
2026-07-02 01:28:25 -0400

    Update map markers (issue #1)

--- map.js
+++ map.js
@@ -1,8 +1,16 @@
+const iconSize = 36
+
 var map = L.map('map').setView([40.418, -86.897], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var flockcam = L.icon({
+    iconUrl: 'images/flock-camera-icon.png',
+    iconSize: [iconSize, iconSize],
+    iconAnchor: [iconSize/2, iconSize]
+})
 
 async function addFlockMarkers() {
     try {
@@ -14,13 +22,19 @@ async function addFlockMarkers() {
 
         while (data.features[i]) {
             let coords = data.features[i].geometry.coordinates;
-            L.marker([coords[1], coords[0]]).addTo(map);
+            L.marker([coords[1], coords[0]], {icon: flockcam}).addTo(map);
             i++;
         }
     } catch (error) {
         console.error('Error reading JSON:', error.message);
     }
 }
+
+var purduecam = L.icon({
+    iconUrl: 'images/purdue-camera-icon.png',
+    iconSize: [iconSize, iconSize],
+    iconAnchor: [iconSize/2, iconSize]
+})
 
 async function addPurdueMarkers() {
     try {
@@ -32,7 +46,7 @@ async function addPurdueMarkers() {
 
         while (data.features[i]) {
             let coords = data.features[i].geometry.coordinates;
-            L.marker([coords[1], coords[0]]).addTo(map);
+            L.marker([coords[1], coords[0]], {icon: purduecam}).addTo(map);
             i++;
         }
     } catch (error) {
