1239f3174d67ab69f13cab431b85dd38cccf1420
Obind30 <obindewald30@gmail.com>
2026-07-17 00:10:14 -0400

    Linear spotlight follow

--- src/scripts/spotlight.js
+++ src/scripts/spotlight.js
@@ -3,3 +3,10 @@ const spotlight = document.getElementById("spotlight");
 
 //Scale the spotlight's aspect ratio to fit to screen (account for resizing eventually)
 spotlight.setAttribute("transform", "scale(" + 1 + "," + window.innerHeight/window.innerWidth + ")");
+
+
+const bredth = 100;
+
+document.addEventListener("mousemove", function(event) {
+    spotlight.setAttribute("transform", "skewX("+ ((event.clientX/window.innerWidth)*bredth-bredth/2)%bredth + ")");
+})
