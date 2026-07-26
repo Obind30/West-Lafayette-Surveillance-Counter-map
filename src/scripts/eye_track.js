541c6d026ec2454ff47c17e56f666439d3c98319
Obind30 <obindewald30@gmail.com>
2026-07-26 18:14:06 -0400

    Traces fill negative space, text added to links

--- src/scripts/eye_track.js
+++ src/scripts/eye_track.js
@@ -6,9 +6,9 @@ pupil_center = [((eye.right-eye.left)/2) + eye.left, ((eye.bottom-eye.top)/2) + 
 onresize = (event) => {
     eye = document.getElementById("eye_icon").getBoundingClientRect();
     pupil_center = [((eye.right-eye.left)/2) + eye.left, ((eye.bottom-eye.top)/2) + eye.top];
+    resize_eye();
 }
 
 document.addEventListener("mousemove", function(event) {
-   console.log(event.clientX-pupil_center[0], event.clientY-pupil_center[1]);
    pupil.setAttribute("transform", "translate(" + (event.clientX-pupil_center[0])/30 + " " + (event.clientY-pupil_center[1])/30 +")")
 });
