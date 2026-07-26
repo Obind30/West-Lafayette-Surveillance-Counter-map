f50c6df8a6ce5dca1c4990b550b9137fad6a6869
Obind30 <obindewald30@gmail.com>
2026-07-26 00:20:54 -0400

    FIX: Eye centering issue

--- src/scripts/eye_track.js
+++ src/scripts/eye_track.js
@@ -1,7 +1,12 @@
 const pupil = document.getElementById("pupil");
 
-const eye = document.getElementById("eye_icon").getBoundingClientRect();
+var eye = document.getElementById("eye_icon").getBoundingClientRect();
 pupil_center = [((eye.right-eye.left)/2) + eye.left, ((eye.bottom-eye.top)/2) + eye.top];
+
+onresize = (event) => {
+    eye = document.getElementById("eye_icon").getBoundingClientRect();
+    pupil_center = [((eye.right-eye.left)/2) + eye.left, ((eye.bottom-eye.top)/2) + eye.top];
+}
 
 document.addEventListener("mousemove", function(event) {
    console.log(event.clientX-pupil_center[0], event.clientY-pupil_center[1]);
