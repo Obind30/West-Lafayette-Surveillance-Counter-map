b8a6d5778ddaca687f41b19b5e0f20ea40785746
Obind30 <obindewald30@gmail.com>
2026-08-30 20:59:23 -0400

    Updated flock camera locations

--- Flock_Geo_Filter.py
+++ Flock_Geo_Filter.py
@@ -11,8 +11,8 @@ def filter_by_coord(object):
     except:
         return False
 
-nationwide_file = open("location_data/National-Flock-Data.geojson")
-local_file = open("location_data/GreaterLAF-Flock-Cameras.geojson", "w")
+nationwide_file = open("src/location_data/National-Flock-Data.geojson")
+local_file = open("src/location_data/GreaterLAF-Flock-Cameras.geojson", "w")
 
 # Filter from full list of cameras
 camera_data = json.load(nationwide_file)
